fa7cf8ec2e2419551f02c06ea3f30ee960820857
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-02-14 17:02:24 +0100

    Refactored CSS, added HTML messages by default. Updated TODO and Changelog.

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -8,5 +8,5 @@ pref("gconversation.button.unfold_all", true);
 pref("gconversation.button.fold_all", true);
 pref("gconversation.button.mark_as_read", true);
 pref("gconversation.button.open_in_new_tab", true);
-pref("gconversation.html", false);
+pref("gconversation.html", true);
 pref("gconversation.focus_first", false);
