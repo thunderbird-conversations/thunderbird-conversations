48da16c710173ecc6df13e3f8e9219d7315e2ed1
Jonathan Protzenko <jonathan.protzenko+github@gmail.com>
2010-04-13 19:51:37 +0200

    Revert "Don't automatically mark a conversation as read if the main window doesn't have focus." --> When one of the messages in the conversation has focus, the focus events are not sent properly accross the main window, which renders the modification useless. Backing out until a better solution is found.
    This reverts commit 0d4c9a0fff37a61e9603068d17be251c5fb4bc45.

--- content/selectionsummaries.js
+++ content/selectionsummaries.js
@@ -58,14 +58,11 @@ var gconversation = {
   archive_all: null,
   delete_all: null,
   print: null,
-  /* Wrap what would be globals */
-  main_window_is_focused: false,
   /* Prevent GC */
   stash: {
     wantedUrl: null,
     q1: null,
-    q2: null,
-    msgHdrs: null
+    q2: null
   }
 };
 
@@ -925,7 +922,7 @@ document.addEventListener("load", function f_temp0 () {
         gSummary = new ThreadSummary(items, aListener);
         gSummary.init();
 
-        if (gPrefs["auto_mark_read"] && gconversation.main_window_is_focused)
+        if (gPrefs["auto_mark_read"])
           gconversation.mark_all_read();
         return;
       }
@@ -1092,7 +1089,7 @@ document.addEventListener("load", function f_temp0 () {
   document.getElementById("multimessage").setAttribute("context", "gConvMenu");
 
   /* Watch the location changes in the messagepane (single message view) to
-   * display a conversation if relevant. This is the "auto-fetch" feature. */
+   * display a conversation if relevant. */
   let messagepane = document.getElementById("messagepane");
   gconversation.stash.uriWatcher = {
     onStateChange: function () {},
@@ -1163,19 +1160,6 @@ document.addEventListener("load", function f_temp0 () {
     QueryInterface: XPCOMUtils.generateQI([Ci.nsISupports, Ci.nsISupportsWeakReference, Ci.nsIWebProgressListener])
   };
   messagepane.addProgressListener(gconversation.stash.uriWatcher);
-
-  /* This is needed to know if we can mark a conversation as read when it is
-   * opened. If we don't do that, then if a conversation is selected, when a new
-   * message arrives in the conversation, the conversation is automatically
-   * reloaded, marked as read instantly, and the user is not notified. */
-  window.addEventListener("focus", function (event) {
-    if (event.target == window)
-      gconversation.main_window_is_focused = true;
-  }, true);
-  window.addEventListener("blur", function (event) {
-    if (event.target == window)
-      gconversation.main_window_is_focused = false;
-  }, true);
 
   myDump("*** gConversation loaded\n");
 
