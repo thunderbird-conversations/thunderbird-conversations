a559b44458976f006b12ea9ddf604b57512916ec
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-08-19 14:04:14 -0700

    New build system

---
+++ config_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=gconversation
+CHROME_PROVIDERS="content locale skin"
+CLEAN_UP=1
+ROOT_FILES="icon.png LICENSE README.md"
+ROOT_DIRS="defaults"
+BEFORE_BUILD=
+AFTER_BUILD=
---
+++ package.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+BRANCH=`git branch | egrep "\\* (.*)" | cut -c 3-`
+DATE=`date +%Y%m%d%H%M`
+TARGET_FILENAME="$DATE-$BRANCH.xpi"
+GNUFILE=/Users/protz/bin/switchtognuutils
+
+if [ -f "$GNUFILE" ]; then
+  . "$GNUFILE";
+fi;
+
+template() {
+  sed s/__REPLACEME__/.$1/ install.rdf.template > install.rdf
+}
+
+upload() {
+  echo "cd jonathan/files\nput ../gconversation.xpi gcv-nightlies/$TARGET_FILENAME\n\
+    put Changelog gcv-nightlies/Changelog_$BRANCH" | ftp xulforum@ftp.xulforum.org
+}
+
+if [ "$1" = "official" ]; then
+  template "";
+  ./build.sh
+  upload;
+else
+  template "$DATE"pre;
+  ./build.sh
+  upload;
+  rm -f gconversation.xpi;
+fi
