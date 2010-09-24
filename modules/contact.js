4864c44d4a42d67cf6ed5d58b97f01a99f6ec329
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2010-09-24 13:28:16 +0200

    More Thunderbird Contacts support.

--- modules/contact.js
+++ modules/contact.js
@@ -79,6 +79,19 @@ let ContactMixIn = {
       ? ""
       : ("color :" + this.color)
     ;
+    let self = this;
+    let replace = function _replace (svc, url) {
+      if (svc in self._profiles) {
+        let r = [
+          "<a href=\"", url.replace("#1", self._profiles[svc]), "\">",
+            "<img src=\"chrome://conversations/content/i/", svc, ".ico\" />",
+          "</a>"
+        ];
+        return r.join("");
+      } else {
+        return "";
+      }
+    };
     let r = [
       "<span class=\"tooltipWrapper\">",
       "<span style=\"", colorStyle, "\">",
@@ -94,6 +107,11 @@ let ContactMixIn = {
       "    <div class=\"authorPicture\">",
       "      <img src=\"", this.avatar, "\">",
       "    </div>",
+      "    <div class=\"authorInfo authorLinks\">",
+            replace("facebook", "http://www.facebook.com/profile.php?id=#1"),
+            //replace("google", "http://www.google.com/profiles/#1"),
+            replace("twitter", "http://www.twitter.com/#1"),
+      "    </div>",
       "    <div class=\"tipFooter\">",
       "      <button class=\"sendEmail\">send email</button>",
       "      <button>more</button>",
@@ -105,6 +123,7 @@ let ContactMixIn = {
   },
 
   onAddedToDom: function _ContactMixIn_onAddedToDom(aDomNode) {
+    /* Register the "send message" link */
     let uri = "mailto:" + this._email;
     let aURI = ioService.newURI(uri, null, null);
     aDomNode.getElementsByClassName("sendEmail")[0].addEventListener(
@@ -112,6 +131,17 @@ let ContactMixIn = {
         msgComposeService.OpenComposeWindowWithURI(null, aURI);
         event.stopPropagation();
       }, false);
+
+    let mainWindow = Cc["@mozilla.org/appshell/window-mediator;1"]
+                            .getService(Ci.nsIWindowMediator)
+                            .getMostRecentWindow("mail:3pane");
+    /* The links to various profiles */
+    for each (let [, a] in Iterator(aDomNode.getElementsByTagName("a"))) {
+      a.addEventListener("click",
+        function _link_listener (event)
+          mainWindow.specialTabs.siteClickHandler(event, /^mailto:/),
+        true);
+    }
   },
 };
 
@@ -123,6 +153,7 @@ function ContactFromAB(manager, name, email) {
   this._manager = manager;
   this._name = name;
   this._email = email; // The original email. Use to pick a gravatar.
+  this._profiles = {};
 
   this.fetch();
 }
@@ -158,14 +189,17 @@ ContactFromAB.prototype = {
 MixIn(ContactFromAB, ContactMixIn);
 
 function ContactFromPeople(manager, name, email) {
-  this.name = name;
+  this.name = name || email;
   this.emails = [email];
   this.color = manager.freshColor();
 
   this._manager = manager;
   this._name = name;
   this._email = email;
-  this.avatar = "";
+  this.avatar = "http://www.gravatar.com/avatar/"
+      + GlodaUtils.md5HashString(this._email.trim().toLowerCase())
+      + "?r=pg&d=wavatar&s=80";
+  this._profiles = {};
 
   this.fetch();
 }
@@ -177,11 +211,34 @@ ContactFromPeople.prototype = {
       Log.debug("Found a match in contacts");
 
       let photos = person.getProperty("photos");
-      for each (let photo in photos) {
-        if (photo.type == "thumbnail") {
-          self.avatar = photo.value;
-          break;
-        }
+      let gravatarPhotos = [photo
+        for each (photo in photos)
+        if (photo.value.indexOf("www.gravatar.com") >= 0)
+      ];
+      let profilePhotos = [photo
+        for each (photo in photos)
+        if (photo.type == "profile")
+      ];
+      let thumbnailPhotos = [photo
+        for each (photo in photos)
+        if (photo.type == "thumbnail")
+      ];
+      if (gravatarPhotos.length)
+        self.avatar = gravatarPhotos[0].value;
+      else if (profilePhotos.length)
+        self.avatar = profilePhotos[0].value;
+      else if (thumbnailPhotos.length)
+        self.avatar = thumbnailPhotos[0].value;
+
+      /* Will set this._profiles = {
+       *  facebook: id,
+       *  google: the email,
+       *  linkedin: the email,
+       *  twitter: the nickname
+       * }
+       * */
+      for each (let [svcName, svc] in Iterator(person.obj.documents)) {
+        self._profiles[svcName] = [k for (k in svc)];
       }
 
       self.name = person.displayName;
