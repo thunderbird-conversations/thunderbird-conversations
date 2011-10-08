1090f12c641bf681c342e3c188c715ac1a24394b
rene <github@absorb.it>
2011-10-08 19:45:18 +0200

    bugfix: while typing new recipient, errors are occur

--- content/stub.completion-ui.js
+++ content/stub.completion-ui.js
@@ -183,7 +183,7 @@ ContactIdentityCompleter.prototype = {
         let contact = items[iContact];
         if (!(contact.id in contactToThing)) {
           contactToThing[contact.id] = contact;
-          possibleDudes.push(contact.identities[0]);
+          if (contact.identities[0]) possibleDudes.push(contact.identities[0]);
         }
       }
 
