ece9d0f6b7157cfe69709fd98686b60ec58ce529
rene <github@absorb.it>
2011-11-09 13:41:31 +0100

    new hook descriptions

--- modules/hook.js
+++ modules/hook.js
@@ -12,9 +12,12 @@ var EXPORTED_SYMBOLS = ['registerHook', 'getHooks', 'removeHook'];
  *  onMessageStreamed (aMsgHdr, aDomNode, aMsgWindow, aMessage) {
  *  },
  *
- *  // Called before the quick reply message is send.
- *  // @param aAddress.identity The identity the user picked to send the
- *  //  message.
+ *  // Called before the quick reply message is send or opened in a new
+ *  // compose-window.
+ *  // Set of functions with different priorities, call to "_early" function
+ *  // should not expect parameters (like sender, recipients) to keep stable.
+ *  // @param aAddress.param The params (sender, identity) the user picked
+ *  //  to send the message.
  *  // @param aAddress.to The recipients. This is an Array of valid email
  *  //  addresses.
  *  // @param aAddress.cc Same remark.
@@ -22,8 +25,11 @@ var EXPORTED_SYMBOLS = ['registerHook', 'getHooks', 'removeHook'];
  *  // @param aEditor The textarea node which value is mail body.
  *  // @param aStatus.canceled Sending the message is canceled.
  *  // @param aStatus.securityInfo An object for PGM/MIME message.
+ *  // @param aPopout if set, message will not be opened in compose window
  *  // @return aStatus Same remark.
- *  onMessageBeforeSend: function (aAddress, aEditor, aStatus) {
+ *  onMessageBeforeSendOrPopout_early: function (aAddress, aEditor, aStatus, aPopout) {
+ *  },
+ *  onMessageBeforeSendOrPopout: function (aAddress, aEditor, aStatus, aPopout) {
  *  },
  *
  *  // Called when quick reply body is composed.
@@ -38,6 +44,29 @@ var EXPORTED_SYMBOLS = ['registerHook', 'getHooks', 'removeHook'];
  *  // @param aMessage Focused message instance.
  *  onFocusMessage (aMessage) {
  *  },
+ * 
+ *  // Called whenever a new quickreply Compose-Session was
+ *  // created and all fields are prepared
+ *  // @param aComposeSession created composeSession
+ *  // @param aMessage current message instance
+ *  // @param aRecipients current recipients (to, cc, bcc)
+ *  onComposeSessionChanged (aComposeSession, aMessage, aRecipients) {
+ *  },
+ * 
+ *  // Called whenever a recipient is added to the quickreply
+ *  // @param aData list data containing user input
+ *  // @param aType type of changed recipient (to, cc, bcc)
+ *  // @param aCount number of recipients of this type
+ *  onRecipientAdded(aData, aType, aCount) {
+ *  },
+ * 
+ *  // Called regardless of whether the Sending operation was successful.
+ *  // @param aMsgID   The message id for the mail message
+ *  // @param aStatus   Status code for the message send.
+ *  // @param aMsg      A text string describing the error.
+ *  // @param aReturnFile The returned file spec for save to file operations.
+ *  onStopSending (aMsgID, aStatus, aMsg, aReturnFile) {
+ *  }
  * }
  *
  * If you need something else, just ask!
