fd00583d64ba9f0eed7cfb49200ae0c6dbfaf1a0
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2012-04-04 16:48:33 +0200

    Package bootstrap.js in the build script.

--- build_config.sh
+++ build_config.sh
@@ -3,7 +3,7 @@
 APP_NAME=conversations
 CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
-ROOT_FILES="icon.png LICENSE README.md"
+ROOT_FILES="icon.png LICENSE README.md bootstrap.js"
 ROOT_DIRS="defaults modules"
 BEFORE_BUILD=
 AFTER_BUILD=
