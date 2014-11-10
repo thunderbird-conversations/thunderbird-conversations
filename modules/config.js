28e26cf9c630c7cee99b91c233d71f69a4189ace
pianoslum <pianoslum@users.noreply.github.com>
2014-11-10 11:00:41 +0900

    Move the BOOTSTRAP_REASONS constants to config.js

--- modules/config.js
+++ modules/config.js
@@ -1,3 +1,14 @@
-var EXPORTED_SYMBOLS = ['conversationsCurrentVersion']
+var EXPORTED_SYMBOLS = ['conversationsCurrentVersion', 'BOOTSTRAP_REASONS']
 
 let conversationsCurrentVersion = 1;
+
+const BOOTSTRAP_REASONS = {
+  APP_STARTUP     : 1,
+  APP_SHUTDOWN    : 2,
+  ADDON_ENABLE    : 3,
+  ADDON_DISABLE   : 4,
+  ADDON_INSTALL   : 5,
+  ADDON_UNINSTALL : 6,
+  ADDON_UPGRADE   : 7,
+  ADDON_DOWNGRADE : 8
+};
