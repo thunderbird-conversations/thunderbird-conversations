d3179283df9d861d1937324fc99aeadc513a4ed4
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-05-01 15:17:53 -0700

    Git rid of useless build script

--- build.sh
+++ build.sh
@@ -1,113 +1,20 @@
-#!/bin/bash
-# build.sh -- builds JAR and XPI files for mozilla extensions
-#   by Nickolay Ponomarev <asqueella@gmail.com>
-#   (original version based on Nathan Yergler's build script)
-# Most recent version is at <http://kb.mozillazine.org/Bash_build_script>
+#!/bin/sh
+NOT='.sh$|^.git|^tests'
+PDFJS=content/pdfjs/build/pdf.js
 
-# This script assumes the following directory structure:
-# ./
-#   chrome.manifest (optional - for newer extensions)
-#   install.rdf
-#   (other files listed in $ROOT_FILES)
-#
-#   content/    |
-#   locale/     |} these can be named arbitrary and listed in $CHROME_PROVIDERS
-#   skin/       |
-#
-#   defaults/   |
-#   components/ |} these must be listed in $ROOT_DIRS in order to be packaged
-#   ...         |
-#
-# It uses a temporary directory ./build when building; don't use that!
-# Script's output is:
-# ./$APP_NAME.xpi
-# ./$APP_NAME.jar  (only if $KEEP_JAR=1)
-# ./files -- the list of packaged files
-#
-# Note: It modifies chrome.manifest when packaging so that it points to 
-#       chrome/$APP_NAME.jar!/*
-
-#
-# default configuration file is ./config_build.sh, unless another file is 
-# specified in command-line. Available config variables:
-APP_NAME=          # short-name, jar and xpi files name. Must be lowercase with no spaces
-CHROME_PROVIDERS=  # which chrome providers we have (space-separated list)
-CLEAN_UP=          # delete the jar / "files" when done?       (1/0)
-ROOT_FILES=        # put these files in root of xpi (space separated list of leaf filenames)
-ROOT_DIRS=         # ...and these directories       (space separated list)
-BEFORE_BUILD=      # run this before building       (bash command)
-AFTER_BUILD=       # ...and this after the build    (bash command)
-
-if [ -z $1 ]; then
-  . ./build_config.sh
-else
-  . $1
-fi
-
-if [ -z $APP_NAME ]; then
-  echo "You need to create build config file first!"
-  echo "Read comments at the beginning of this script for more info."
-  exit;
-fi
+git ls-files | egrep -v $NOT > files
+for a in $(cd modules/stdlib && git ls-files | egrep -v $NOT); do
+  echo modules/stdlib/$a >> files
+done
 
 if [ -f "content/pdfjs/build/pdf.js" ]; then
   true;
 else
   echo "Please run make from content/pdfjs";
-  exit 0
+  exit 1
 fi
+echo $PDFJS >> files
 
-ROOT_DIR=`pwd`
-TMP_DIR=build
-
-set -e
-
-# remove any left-over files from previous build
-rm -f $APP_NAME.xpi files
-rm -rf $TMP_DIR
-
-$BEFORE_BUILD
-
-mkdir -pv $TMP_DIR
-
-# copy everything brutally, since we're not using JARs anymore
-for DIR in $ROOT_DIRS; do
-  find $DIR -not \( -wholename $DIR'/pdfjs/*' \
-        -and -not -wholename $DIR'/pdfjs/build/*' \) \
-      -and -not -iname '.vimsession' \
-      -and -not -iname '.gitignore' \
-      -and -not -iname '.*.sw*' \
-      -and -not -iname '.sw*' \
-    -type f -print | grep -v \~ | grep -v '/.git/' >> files
-done
-
-# The following statement should be used instead if you don't wish to use the JAR file
-cp -pv `cat files` $TMP_DIR
-
-# Copy other files to the root of future XPI.
-for ROOT_FILE in $ROOT_FILES install.rdf chrome.manifest; do
-  cp -v $ROOT_FILE $TMP_DIR
-  if [ -f $ROOT_FILE ]; then
-    echo $ROOT_FILE >> files
-  fi
-done
-
-cd $TMP_DIR
-find . -iname 'tests' -type d -exec rm -rf {} \;
-
-# generate the XPI file
-echo "Generating $APP_NAME.xpi..."
-zip -r ../$APP_NAME.xpi *
-
-cd "$ROOT_DIR"
-
-echo "Cleanup..."
-if [ $CLEAN_UP != 0 ]; then
-  rm ./files
-fi
-
-# remove the working files
-rm -rf $TMP_DIR
-echo "Done!"
-
-$AFTER_BUILD
+rm conversations.xpi
+zip conversations.xpi $(cat files)
+rm files
