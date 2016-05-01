1bdad4b25c80821222ea1597cc6363a489bc5365
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-05-01 15:17:53 -0700

    Git rid of useless build script

--- build_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build config for build.sh
-APP_NAME=conversations
-CLEAN_UP=1
-ROOT_FILES="icon.png LICENSE README.md bootstrap.js"
-ROOT_DIRS="content locale skin defaults modules"
-BEFORE_BUILD=
-AFTER_BUILD=
