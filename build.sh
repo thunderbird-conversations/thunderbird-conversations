9704a57b4fa5454e74498efdf97544a3fd0b74eb
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-05-24 23:34:22 -0700

    Fix gallery view and pdf preview

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 NOT='.sh$|^.git|^tests'
 PDFJS=content/pdfjs/build/pdf.js
+PDFWORKERJS=content/pdfjs/build/pdf.worker.js
 
 git ls-files | egrep -v $NOT > files
 for a in $(cd modules/stdlib && git ls-files | egrep -v $NOT); do
   echo modules/stdlib/$a >> files
 done
 
-if [ -f "content/pdfjs/build/pdf.js" ]; then
+if [ -f $PDFJS ]; then
   true;
 else
   echo "Please run make from content/pdfjs";
   exit 1
 fi
 echo $PDFJS >> files
+echo $PDFWORKERJS >> files
 
 rm conversations.xpi
 zip conversations.xpi $(cat files)
