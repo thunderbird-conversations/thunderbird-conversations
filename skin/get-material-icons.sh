f2df6a6edac022f1a7ffb4fd4fdf398ac5a0f999
Flying Lollipop <fllp@posteo.org>
2017-03-04 13:23:01 +0100

    Fetch additional material icons

--- skin/get-material-icons.sh
+++ skin/get-material-icons.sh
@@ -8,7 +8,7 @@ fi
 
 echo '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">'
 echo '    <!-- Material icons are published under Apache License Version 2.0. https://material.io/icons/ -->'
-for icon in "archive" "attachment" "code" "content_copy" "delete" "edit" "vpn_key" "expand_less" "expand_more" "forward" "inbox" "info_outline" "whatshot" "list" "more_vert" "open_in_new" "print" "reply_all" "reply" "star" "visibility" "visibility_off" "warning"; do
+for icon in "archive" "attachment" "code" "content_copy" "delete" "edit" "vpn_key" "expand_less" "expand_more" "forward" "inbox" "info_outline" "whatshot" "list" "more_vert" "open_in_new" "print" "reply_all" "reply" "star" "visibility" "visibility_off" "warning" "info" "add" "mail" "history" "file_download" "photo_library" "search" "account" "account_circle"; do
     wget -q "https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_${icon}_black_24px.svg" -O - \
         | grep -v fill | $HEAD -n -1 \
         | sed "s/<path/<path id=\"${icon}\"/"
