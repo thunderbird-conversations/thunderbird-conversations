71019284a9f83566c2e496ad79ccffb736ec0b76
Mark Banner <standard8@mozilla.com>
2019-01-02 21:42:18 +0000

    Switch to using jsx for react components.

--- build.sh
+++ build.sh
@@ -1,23 +1,44 @@
 #!/bin/sh
-NOT='.sh$|^.git|^tests|^.eslint|^.travis|^package.json$|^package-lock.json$'
-VENDOR_DIR=content/vendor
+NOT='.sh$|^.git|.jsx$|^tests|^.eslint|^.travis|^package.json$|^package-lock.json$'
+DIST=dist
+VENDOR_DIR=$DIST/content/vendor
 
+rm -rf $DIST
 mkdir -p $VENDOR_DIR
-rm -r $VENDOR_DIR/*.*
+mkdir -p $DIST/modules/stdlib
+
 cp node_modules/react/umd/react.production.min.js $VENDOR_DIR/react.js
 cp node_modules/react-dom/umd/react-dom.production.min.js $VENDOR_DIR/react-dom.js
+cp node_modules/prop-types/prop-types.min.js $VENDOR_DIR/prop-types.js
 cp node_modules/pdfjs-dist/build/pdf.js $VENDOR_DIR
 cp node_modules/pdfjs-dist/build/pdf.worker.js $VENDOR_DIR
 
 git ls-files | egrep -v $NOT > files
+
+for a in $(git ls-files | grep '.jsx$'); do
+  echo $a
+  babel $a --out-dir $DIST/$(dirname $a)
+  echo ${a%.*}.js >> files
+done
+
+for a in $(git ls-files | egrep -v $NOT | egrep -v '^modules/stdlib'); do
+  mkdir -p $(dirname "${DIST}/${a}")
+  cp $a $DIST/$a
+done
+
 for a in $(cd modules/stdlib && git ls-files | egrep -v $NOT); do
-  echo modules/stdlib/$a >> files
+  if [ $a != "" ]; then
+    cp modules/stdlib/$a $DIST/modules/stdlib
+    echo modules/stdlib/$a >> files
+  fi
 done
 
 for a in $(cd $VENDOR_DIR && ls); do
-  echo $VENDOR_DIR/$a >> files
+  echo content/vendor/$a >> files
 done
 
-rm conversations.xpi
-zip conversations.xpi $(cat files)
+rm -f conversations.xpi
+pushd dist
+zip ../conversations.xpi $(cat ../files)
+popd
 rm files
