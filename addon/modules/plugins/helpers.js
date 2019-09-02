6c9501f2869dca77487dc638d04ed4f51a78c751
Mark Banner <standard8@thebanners.uk>
2019-09-02 09:10:18 +0100

    Remove old MoMo hooks

--- addon/modules/plugins/helpers.js
+++ addon/modules/plugins/helpers.js
@@ -13,44 +13,26 @@ var EXPORTED_SYMBOLS = ["PluginHelpers"];
 
 const {GlodaUtils} = ChromeUtils.import("resource:///modules/gloda/utils.js");
 
-const gsfnRegexp = /^(.+)(?:, an employee of Mozilla Messaging,)? (?:replied to|commented on|just asked)/;
-const gsfnFrom = "Mozilla Messaging <noreply.mozilla_messaging@getsatisfaction.com>";
-
 var PluginHelpers = {
   // About to do more special-casing here? Please check out the corresponding
   //  code in contact.js and make sure you modify it too.
-  alternativeSender: function _PluginHelpers_alternativeSender(aRawReps) {
-    let aMimeMsg = aRawReps.mime;
-    let aMsgHdr = aRawReps.header;
+  alternativeSender(aRawReps) {
+    const aMimeMsg = aRawReps.mime;
 
     // This header is a bare email address
     if (aMimeMsg && ("x-bugzilla-who" in aMimeMsg.headers))
         return aMimeMsg.headers["x-bugzilla-who"];
 
-    // The thing is, the template caches the contacts according to their
-    // emails, so we need to make sure the email address is unique for each
-    // person (otherwise Person A <email> is cached with email as the key, and
-    // Person B <sameemail> appears as Person A. See contact.js
-    let uniq = s => GlodaUtils.md5HashString(s).substring(0, 8);
-
-    // We sniff for a name
-    if (aMimeMsg && aMimeMsg.headers.from == gsfnFrom) {
-      let body = aMimeMsg.coerceBodyToPlaintext(aMsgHdr.folder);
-      let m = body.match(gsfnRegexp);
-      if (m && m.length)
-        return (m[1] + " <" + uniq(m[1]) + "@fake.getsatisfaction.com>");
-    }
-
     return null;
   },
 
-  bugzilla: function _PluginHelpers_bugzilla(aRawReps) {
-    let aMimeMsg = aRawReps.mime;
+  bugzilla(aRawReps) {
+    const aMimeMsg = aRawReps.mime;
     if (!aMimeMsg)
       return null;
 
     if (aMimeMsg.has("x-bugzilla-who")) {
-      let keys = [
+      const keys = [
         "url",
         "classification",
         "product",
@@ -63,9 +45,9 @@ var PluginHelpers = {
         "target-milestone",
         "changed-fields",
       ];
-      let o = {};
-      for (let k of keys) {
-        let v = aMimeMsg.get("x-bugzilla-" + k);
+      const o = {};
+      for (const k of keys) {
+        const v = aMimeMsg.get("x-bugzilla-" + k);
         if (v)
           o[k] = GlodaUtils.deMime(v);
       }
