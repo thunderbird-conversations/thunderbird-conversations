b6ad094ef8a461c3a7831dfe14b52f18e40c0765
Mark Banner <standard8@thebanners.uk>
2019-09-02 22:05:07 +0100

    Fixes #1364 - Transition preferences display to WebExtension style, to fix display them in Thunderbird 68.

--- addon/defaults/preferences/defaults.js
+++ addon/defaults/preferences/defaults.js
@@ -3,7 +3,6 @@ pref("conversations.hide_quote_length", 5);
 pref("conversations.expand_who", 4); // kExpandAuto
 pref("conversations.monospaced_senders", "bugzilla-daemon@mozilla.org");
 pref("conversations.no_friendly_date", false);
-pref("conversations.nruns", 0);
 pref("conversations.uninstall_infos", "{}");
 pref("conversations.logging_enabled", false);
 pref("conversations.tweak_bodies", true);
