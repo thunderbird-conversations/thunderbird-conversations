c6bca977f957020197f53cef398ebb1312162d41
Jason Siefken <siefkenj@gmail.com>
2019-12-15 21:23:06 -0500

    Reduced `options.html` to a single import

--- addon/content/es-modules/ui.js
+++ addon/content/es-modules/ui.js
@@ -2,29 +2,48 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// We assume all these have been loaded as globals before this module is executed
-for (const lib of [
-  "React",
-  "ReactDOM",
-  "Redux",
-  "ReactRedux",
-  "RTK",
-  "PropTypes",
-]) {
-  if (!window[lib]) {
-    console.warn(
-      "Assumed",
-      lib,
-      `was a globally accessible library, but window.${lib} is not defined.`
+// `amdModules` will be an array of AMD module initialization information.
+// It is up to us to initialize all the modules in the proper order and re-export them.
+import { amdModules, callWithDeps } from "./modules-compat.js";
+
+// Load vendor modules as AMD modules
+// IMPORTANT: the order here must be kept in sync
+// with `amdModuleNames` and the order must resolve deps.
+// e.g., `react-dom` must come after `react`, since `react` is a
+// dependency of `react-dom`
+import "../vendor/react.js";
+import "../vendor/react-dom.js";
+import "../vendor/redux.js";
+import "../vendor/react-redux.js";
+import "../vendor/redux-toolkit.umd.js";
+import "../vendor/prop-types.js";
+
+const initializedDeps = {};
+
+// These names must be in the same order as the AMD modules were imported
+const amdModuleNames = [
+  "react",
+  "react-dom",
+  "redux",
+  "react-redux",
+  "redux-toolkit",
+  "prop-types",
+];
+amdModuleNames.forEach((name, i) => {
+  const amdItem = amdModules[i];
+  if (!amdItem) {
+    throw new Error(
+      `An ${i}th AMD module was assumed to be loaded, but none was found`
     );
   }
-}
+  initializedDeps[name] = callWithDeps(amdItem, initializedDeps);
+});
 
-const React = window.React;
-const ReactDOM = window.ReactDOM;
-const Redux = window.Redux;
-const ReactRedux = window.ReactRedux;
-const RTK = window.RTK;
-const PropTypes = window.PropTypes;
+const React = initializedDeps["react"];
+const ReactDOM = initializedDeps["react-dom"];
+const Redux = initializedDeps["redux"];
+const ReactRedux = initializedDeps["react-redux"];
+const RTK = initializedDeps["redux-toolkit"];
+const PropTypes = initializedDeps["prop-types"];
 
 export { React, ReactDOM, Redux, ReactRedux, RTK, PropTypes };
