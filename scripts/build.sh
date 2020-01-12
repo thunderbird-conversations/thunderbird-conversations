ad92b11612d5ca4836c1c9b7e9e7b51b80d760f2
Jason Siefken <siefkenj@gmail.com>
2020-01-12 16:09:00 -0500

    Removed excess reference to `__test__` directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-NOT='.sh$|^.git|.jsx$|^tests|^.eslint|^.travis|^package.json$|^package-lock.json$|^__test__'
+NOT='.sh$|^.git|.jsx$|^tests|^.eslint|^.travis|^package.json$|^package-lock.json$'
 DIST=dist
 VENDOR_DIR=$DIST/content/vendor
 ADDON_DIR=addon
