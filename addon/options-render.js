4328b2acfcc0cac11e8bfed4e37b19d113802609
Jason Siefken <siefkenj@gmail.com>
2020-01-12 16:51:48 -0500

    Removed inline script in options.html
    	Inline scripts are blocked by the content security policy

---
+++ addon/options-render.js
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { React, ReactDOM } from "./content/es-modules/ui.js";
+import { Main } from "./options.js";
+
+// Render the options to the root of the page
+ReactDOM.render(
+  React.createElement(Main, null),
+  document.querySelector("#root")
+);
