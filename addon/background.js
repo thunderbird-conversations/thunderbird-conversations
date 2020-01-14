d1c39f27e4b27312c93e70d6a04713b7c74965b8
Mark Banner <standard8@thebanners.uk>
2020-01-14 12:38:37 +0000

    Part of #1423: Move quickCompose key setup out of bootstrap, and make it a proper command key on the WebExtension.

--- addon/background.js
+++ addon/background.js
@@ -7,9 +7,35 @@ import { Prefs } from "./prefs.js";
 class Background {
   constructor() {
     this._prefs = new Prefs();
+    this._keyHandler = new KeyHandler();
   }
   async init() {
     await this._prefs.init();
+    await this._keyHandler.init();
+  }
+}
+
+class KeyHandler {
+  init() {
+    browser.commands.onCommand.addListener(command => {
+      if (command == "quick_compose") {
+        console.warn("Quick Compose is currently disabled");
+        // The title/description for this pref is really confusing, we should
+        // reconsider it when we re-enable.
+        // if (Prefs.compose_in_tab) {
+        //   window.openTab("chromeTab", {
+        //     chromePage:
+        //       "chrome://conversations/content/stub.xhtml?quickCompose=1",
+        //   });
+        // } else {
+        //   window.open(
+        //     "chrome://conversations/content/stub.xhtml?quickCompose=1",
+        //     "",
+        //     "chrome,width=1020,height=600"
+        //   );
+        // }
+      }
+    });
   }
 }
 
