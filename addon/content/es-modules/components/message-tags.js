2a6f4d492f45aa4743da8a0e892a8521387e24d6
Jason Siefken <siefkenj@gmail.com>
2020-02-29 17:31:26 -0500

    Update addon/content/es-modules/components/message-tags.js
    Co-Authored-By: Mark Banner <standard8@mozilla.com>

--- addon/content/es-modules/components/message-tags.js
+++ addon/content/es-modules/components/message-tags.js
@@ -3,10 +3,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 /*
- * This is a temporary wrapper. XUL doesn't allow es-modules, but eventually
- * all components should be exported as es-modules. This code "re-exports"
- * globals as es-modules, which enables them to be imported (e.g. for tests)
- * or used as globals.
+ * This is a temporary wrapper. Privileged code doesn't allow es-modules, but
+ * eventually all components should be exported as es-modules. This code 
+ * "re-exports" globals as es-modules, which enables them to be imported
+ * (e.g. for tests) or used as globals.
  *
  * When the switch to a WebExtension is done, the actual code should be migrated
  * here and the global variable workarounds should be removed.
