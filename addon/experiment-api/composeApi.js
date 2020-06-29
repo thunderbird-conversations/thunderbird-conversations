618449b3f6c1ffe20ea59a54f4e70339f3e4bff7
Mark Banner <standard8@mozilla.com>
2020-06-29 12:23:50 +0100

    Add an new, very basic compose in a tab UI.

---
+++ addon/experiment-api/composeApi.js
@@ -0,0 +1,169 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+var { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  ExtensionCommon: "resource://gre/modules/ExtensionCommon.jsm",
+  MailServices: "resource:///modules/MailServices.jsm",
+});
+
+/* exported convCompose */
+var convCompose = class extends ExtensionCommon.ExtensionAPI {
+  getAPI(context) {
+    return {
+      convCompose: {
+        async send(details) {
+          let compFields = Cc[
+            "@mozilla.org/messengercompose/composefields;1"
+          ].createInstance(Ci.nsIMsgCompFields);
+          let params = Cc[
+            "@mozilla.org/messengercompose/composeparams;1"
+          ].createInstance(Ci.nsIMsgComposeParams);
+          let msgSend = Cc[
+            "@mozilla.org/messengercompose/send;1"
+          ].createInstance(Ci.nsIMsgSend);
+          params.composeFields = compFields;
+
+          let sendIdentity;
+          for (let account of MailServices.accounts.accounts) {
+            for (let identity of account.identities) {
+              if (identity.key == details.from) {
+                sendIdentity = identity;
+                break;
+              }
+            }
+            if (sendIdentity) {
+              break;
+            }
+          }
+
+          if (!sendIdentity) {
+            throw new Error("Could not find the specified identity");
+          }
+
+          compFields.from = sendIdentity.email;
+          compFields.to = details.to;
+          compFields.subject = details.subject;
+
+          return new Promise((resolve, reject) => {
+            let sendStatus;
+            let copyListener = {
+              // nsIMsgSendListener
+              onStartSending(aMsgID, aMsgSize) {
+                console.log("Start sending!");
+              },
+              onProgress(aMsgID, aProgress, aProgressMax) {
+                console.log("on progress!");
+              },
+              onStatus(aMsgID, aMsg) {
+                console.log("status", aMsgID, aMsg);
+              },
+              onStopSending(aMsgID, aStatus, aMsg, aReturnFile) {
+                console.log("stop sending", aStatus);
+                sendStatus = aStatus;
+              },
+              onGetDraftFolderURI(aFolderURI) {},
+              onSendNotPerformed(aMsgID, aStatus) {
+                console.log("bad send!");
+                reject(aStatus);
+              },
+              // nsIMsgCopyServiceListener
+              OnStartCopy() {
+                console.log("onStartCopy");
+              },
+              SetMessageKey() {
+                console.log("setMessageKey");
+              },
+              GetMessageId() {
+                console.log("getMessageId");
+              },
+              OnStopCopy() {
+                console.log("onStopCopy");
+                resolve(sendStatus);
+              },
+              QueryInterface: ChromeUtils.generateQI([
+                "nsIMsgSendListener",
+                "nsIMsgCopyServiceListener",
+              ]),
+            };
+
+            let msgProgress = {
+              processCanceledByUser: false,
+              openProgressDialog() {
+                console.log("openProgressDialog");
+              },
+              closeProgressDialog() {
+                console.log("closeProgressDialog");
+              },
+              registerListener() {
+                console.log("registerListener");
+              },
+              unregisterListener() {
+                console.log("unregisterListener");
+              },
+              onStateChange(webProgress, request, stateFlags, status) {
+                console.log("state change", stateFlags, status);
+              },
+              onProgressChange(
+                webProgress,
+                request,
+                self,
+                selfMax,
+                total,
+                totalMax
+              ) {
+                console.log("onProgressChange", self, selfMax, total, totalMax);
+              },
+              onLocationChange() {
+                console.log("onLocationChange");
+              },
+              onStatusChange(webProgress, request, status, message) {
+                console.log("onStatusChange", status, message);
+              },
+              onSecurityChange() {
+                console.log("onSecurityChange");
+              },
+              onContentBlockingEvent() {
+                console.log("onContentBlockingEvent");
+              },
+            };
+
+            let body = details.body || "";
+            if (!body.endsWith("\n")) {
+              body += "\n";
+            }
+
+            try {
+              msgSend.createAndSendMessage(
+                null,
+                sendIdentity,
+                "",
+                compFields,
+                false,
+                false,
+                Ci.nsIMsgSend.nsMsgDeliverNow,
+                null,
+                "text/plain",
+                body,
+                null,
+                null,
+                null,
+                msgProgress,
+                copyListener,
+                null,
+                "",
+                Ci.nsIMsgCompType.New
+              );
+            } catch (ex) {
+              reject(ex);
+            }
+          });
+        },
+      },
+    };
+  }
+};
---
+++ addon/tests/compose.test.jsx
@@ -0,0 +1,184 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+/* eslint-env jest */
+
+// Standard imports for all tests
+const {
+  esmImport,
+  enzyme,
+  React,
+  waitForComponentToPaint,
+  browser,
+} = require("./utils");
+
+// Import the components we want to test
+const { Main, store, actions } = esmImport("../compose/compose.js");
+const { TextArea, TextBox } = esmImport("../compose/composeFields.js");
+
+describe("Compose components have correct return values", () => {
+  test("TextBox always returns a string type", () => {
+    const callback = jest.fn();
+    const option = enzyme.shallow(
+      <TextBox
+        disabled={false}
+        onChange={callback}
+        name="option_name"
+        value={"first text"}
+      />
+    );
+    option
+      .find("input")
+      .simulate("change", { target: { value: "my special text" } });
+
+    expect(callback.mock.calls[0][0]).toBe("option_name");
+    expect(callback.mock.calls[0][1]).toBe("my special text");
+    expect(typeof callback.mock.calls[0][1]).toBe("string");
+  });
+
+  test("TextArea always returns a string type", () => {
+    const callback = jest.fn();
+    const option = enzyme.shallow(
+      <TextArea onChange={callback} name="option_name" value={"first text"} />
+    );
+    option
+      .find("textarea")
+      .simulate("change", { target: { value: "my special text" } });
+
+    expect(callback.mock.calls[0][0]).toBe("option_name");
+    expect(callback.mock.calls[0][1]).toBe("my special text");
+    expect(typeof callback.mock.calls[0][1]).toBe("string");
+  });
+});
+
+describe("Compose Reducer and Actions tests", () => {
+  let mockedList;
+  let mockedGet;
+  let mockedSend;
+
+  beforeEach(() => {
+    mockedList = jest.spyOn(browser.accounts, "list");
+    mockedGet = jest.spyOn(browser.accounts, "get");
+    mockedSend = jest.spyOn(browser.convCompose, "send");
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("initCompose() retrieves the default identity information", async () => {
+    await store.dispatch(actions.initCompose());
+
+    expect(mockedList).toHaveBeenCalled();
+    expect(mockedGet).not.toHaveBeenCalled();
+
+    // Should have correctly set up the initial values.
+    expect(store.getState()).toStrictEqual({
+      from: "id3@example.com",
+      identityId: "id3",
+      email: "id3@example.com",
+      modified: false,
+      os: "win",
+      sending: false,
+      sendingMsg: "",
+    });
+  });
+
+  test("setValue() sets a value in the store", async () => {
+    await store.dispatch(actions.setValue("_custom", "test"));
+
+    expect(store.getState()).toHaveProperty("_custom", "test");
+  });
+
+  test("sendMessage() sends a message", async () => {
+    await store.dispatch(actions.setValue("to", "me@example.com"));
+    await store.dispatch(actions.setValue("subject", "Test"));
+    await store.dispatch(actions.setValue("body", "Hello"));
+    await store.dispatch(actions.sendMessage("custom"));
+
+    expect(mockedSend).toHaveBeenCalledWith({
+      from: "id3",
+      to: "me@example.com",
+      subject: "Test",
+      body: "Hello",
+    });
+  });
+});
+
+describe("Compose full page tests", () => {
+  let mockedSend;
+  let main;
+
+  beforeEach(() => {
+    mockedSend = jest.spyOn(browser.convCompose, "send");
+    main = enzyme.mount(<Main />);
+
+    waitForComponentToPaint(main);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("A message can be sent", async () => {
+    const inputs = main.find(TextBox);
+    for (let i = 0; i < inputs.length; i++) {
+      const inputBox = inputs.at(i);
+      const name = inputBox.props().name;
+      if (name != "from") {
+        inputBox.find("input").simulate("change", { target: { value: name } });
+      }
+    }
+
+    const textArea = main.find(TextArea).at(0);
+    textArea
+      .find("textarea")
+      .simulate("change", { target: { value: "testArea" } });
+
+    const sendButton = main.find("button");
+    sendButton.simulate("click");
+
+    await new Promise((resolve) => {
+      let maxTimes = 10;
+      function tryIt() {
+        if (mockedSend.mock.calls.length) {
+          resolve();
+          return;
+        }
+        maxTimes--;
+        if (!maxTimes) {
+          resolve();
+        }
+        setTimeout(tryIt, 50);
+      }
+      setTimeout(tryIt, 50);
+    });
+
+    expect(mockedSend).toHaveBeenCalledWith({
+      from: "id3",
+      to: "to",
+      subject: "subject",
+      body: "testArea",
+    });
+  });
+
+  test("Modifying a field sets the modififed flag", async () => {
+    await store.dispatch(actions.resetStore());
+
+    const inputs = main.find(TextBox);
+    const inputBox = inputs.at(0);
+    inputBox.find("input").simulate("change", { target: { value: "a" } });
+
+    // Should have correctly set up the initial values.
+    expect(store.getState()).toStrictEqual({
+      from: "a",
+      body: undefined,
+      modified: true,
+      subject: undefined,
+      to: undefined,
+      os: "win",
+      sending: false,
+      sendingMsg: "",
+    });
+  });
+});
