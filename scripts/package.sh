1b65564dc8f4666290215e5165e1e5b8ad6e086c
Mark Banner <standard8@mozilla.com>
2020-07-06 17:46:30 +0100

    Remove old packaging script

--- scripts/package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-BRANCH=`git branch | egrep "\\* (.*)" | cut -c 3-`
-DATE=`date +%Y%m%d%H%M`
-TARGET_FILENAME="$DATE-$BRANCH.xpi"
-
-./scripts/build.sh
-scp conversations.xpi jonathan@protzenko.fr:~/Web/jonathan/thunderbird-conversations/$TARGET_FILENAME
-rm -f conversations.xpi
