89afc4bc5489d06f362a8f91b8b96db22630dfd1
Mark Banner <standard8@mozilla.com>
2020-11-23 12:49:06 +0000

    Fixes #1562: Make message display APIs work properly with Conversations installed.

--- addon/experiment-api/msgWindowApi.js
+++ addon/experiment-api/msgWindowApi.js
@@ -577,6 +577,8 @@ function summarizeThreadHandler(win, id) {
             // TODO: Re-enable this.
             // htmlpane.contentWindow.registerQuickReply();
 
+            win.gMessageDisplay.onLoadCompleted();
+
             // Make sure we respect the user's preferences.
             if (Services.prefs.getBoolPref("mailnews.mark_message_read.auto")) {
               win.conversationsMarkReadTimeout = win.setTimeout(
@@ -695,6 +697,7 @@ function summarizeThreadHandler(win, id) {
         // for this.folderDisplay.selectedCount and returns immediately if
         // selectedCount == 1
         this.singleMessageDisplay = false;
+        this.onDisplayingMessage(this.folderDisplay.selectedMessages[0]);
         win.summarizeThread(this.folderDisplay.selectedMessages, this);
         return true;
       }
