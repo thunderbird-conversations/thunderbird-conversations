aa27c56ee9e79e09160aa67ad1bad4a4f8b2e979
Mark Banner <standard8@mozilla.com>
2021-06-05 16:36:11 +0100

    Move test for freshColor into a contactManager test file to better match where it is.

--- addon/tests/utils.test.jsx
+++ addon/tests/utils.test.jsx
@@ -1,24 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-/* eslint-env jest */
 
-// From https://gist.github.com/olmokramer/82ccce673f86db7cda5e
-function isValidColor(color) {
-  if (color.charAt(0) === "#") {
-    color = color.substring(1);
-    return (
-      [3, 4, 6, 8].indexOf(color.length) > -1 && !isNaN(parseInt(color, 16))
-    );
-  }
-  return /^(rgb|hsl)a?\((\d+%?(deg|rad|grad|turn)?[,\s]+){2,3}[\s\/]*[\d\.]+%?\)$/i.test(
-    color
-  );
-}
-
-// Import the functions we want to test
 import { getInitials } from "../content/es-modules/utils.js";
-import { freshColor } from "../background/contact-manager.js";
 
 describe("Test utility functions", () => {
   test("getInitials extracts initials from names", async () => {
@@ -36,13 +20,5 @@ describe("Test utility functions", () => {
     expect(getInitials("sam@fake.com")).toBe("SA");
     expect(getInitials("same.wise@fake.com")).toEqual("SW");
     expect(getInitials("same.wise+extra@fake.com")).toEqual("SW");
-  });
-  test("freshColor turns an email into a valid hsl color", async () => {
-    // Outputs a valid css color
-    expect(isValidColor(freshColor("abc@fake.com"))).toBe(true);
-    // Outputs the same color for the same email
-    expect(freshColor("abc@fake.com")).toBe(freshColor("abc@fake.com"));
-    // Outputs different colors for different emails
-    expect(freshColor("abc@fake.com")).not.toBe(freshColor("cbc@fake.com"));
   });
 });
