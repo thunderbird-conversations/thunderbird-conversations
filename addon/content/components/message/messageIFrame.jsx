b8b9b05ec8bba818dacfb64fbcb82810e6b12da2
Mark Banner <standard8@mozilla.com>
2021-09-04 13:08:36 +0100

    Fix printing messages not at the top of the conversation as well.

--- addon/content/components/message/messageIFrame.jsx
+++ addon/content/components/message/messageIFrame.jsx
@@ -265,7 +265,7 @@ export class MessageIFrame extends React.Component {
     );
     this.iframe.setAttribute("style", "height: 20px; overflow-y: hidden");
     this.iframe.setAttribute("type", "content");
-    this.iframe.setAttribute("id", "msg");
+    this.iframe.setAttribute("id", `convIframe${this.props.id}`);
     this.div.appendChild(this.iframe);
 
     const docShell = this.iframe.contentWindow.docShell;
