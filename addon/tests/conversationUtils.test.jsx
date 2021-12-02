1b4be5774bc015c7b193779997a6c99727e1b67a
Mark Banner <standard8@mozilla.com>
2021-12-02 14:31:07 +0000

    Fixes: Prevent intermittent failure in tests to do with date and improve structure of fake message creation.

--- addon/tests/conversationUtils.test.jsx
+++ addon/tests/conversationUtils.test.jsx
@@ -20,27 +20,31 @@ describe("conversationUtils", () => {
 
   describe("forward", () => {
     test("Fills out the message with details from the header", async () => {
-      await conversationUtils.forward(0, [
-        createFakeData(
-          {
-            from: {
-              avatar: "avatar.jpg",
-              colorStyle: {
-                backgroundColor: "green",
-              },
-              email: "me@example.com",
-              name: "foo",
+      let now = new Date();
+      let fullDate = new Intl.DateTimeFormat(undefined, {
+        timeStyle: "long",
+      }).format(now);
+      let msg = createFakeData(
+        {
+          from: {
+            avatar: "avatar.jpg",
+            colorStyle: {
+              backgroundColor: "green",
             },
-            fullDate: "2021-08-07 18:14",
+            email: "me@example.com",
+            name: "foo",
           },
-          new Map(),
-          true
-        ),
-      ]);
+        },
+        new Map(),
+        true
+      );
+      msg.fullDate = fullDate;
+
+      await conversationUtils.forward(0, [msg]);
 
       expect(composeSpy).toHaveBeenCalled();
       expect(composeSpy.mock.calls[0][0]).toStrictEqual({
-        body: '<html><body><p>Here\'s a conversation I thought you might find interesting!</p><div style="border-top: 1px solid #888; height: 15px; width: 70%; margin: 0 auto; margin-top: 15px">&nbsp;</div><div style="font-family: sans-serif !important;"><div style="overflow: auto"><img src="avatar.jpg" style="float: left; height: 48px; margin-right: 5px" /><b><span><a style="color: green !important; text-decoration: none !important; font-weight: bold" href="mailto:me@example.com">foo</a></span></b><br /><span style="color: #666">2021-08-07 18:14</span></div><div style="color: #666"><br>MsgBody</div></div>',
+        body: `<html><body><p>Here\'s a conversation I thought you might find interesting!</p><div style="border-top: 1px solid #888; height: 15px; width: 70%; margin: 0 auto; margin-top: 15px">&nbsp;</div><div style="font-family: sans-serif !important;"><div style="overflow: auto"><img src="avatar.jpg" style="float: left; height: 48px; margin-right: 5px" /><b><span><a style="color: green !important; text-decoration: none !important; font-weight: bold" href="mailto:me@example.com">foo</a></span></b><br /><span style="color: #666">${fullDate}</span></div><div style="color: #666"><br>MsgBody</div></div>`,
         identityId: "idac34",
         isPlainText: false,
       });
