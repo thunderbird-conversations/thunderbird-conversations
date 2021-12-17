9949b481d385e4b9709c34726d4bdc5aa3824ee7
Mark Banner <standard8@mozilla.com>
2021-12-17 14:35:06 +0000

    Fixes #1766: Load more scripts in the stub wrapper to allow clicking links in the new message window to work.

--- addon/content/stubWrapper.js
+++ addon/content/stubWrapper.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 window.addEventListener(
   "load",
