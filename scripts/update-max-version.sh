22ee7a1c209f62635e227bd1610be5adb42c3d39
Mark Banner <standard8@mozilla.com>
2025-04-08 10:21:42 +0100

    fix: Make the update max version script more reliable.

--- scripts/update-max-version.sh
+++ scripts/update-max-version.sh
@@ -2,12 +2,17 @@
 
 ADDON="."
 
-THUNDERBIRD_VERSION=`curl -s --fail-with-body https://hg.mozilla.org/comm-central/raw-file/tip/mail/config/version_display.txt`
+THUNDERBIRD_VERSION=`curl -L -s --fail-with-body https://hg.mozilla.org/comm-central/raw-file/tip/mail/config/version_display.txt`
 if [[ $? -ne 0 ]]; then
 	echo CURL FAILED
   exit 1
 fi
 echo $THUNDERBIRD_VERSION
+
+if [[ $THUNDERBIRD_VERSION == "" ]]; then
+  echo Failed to get Thunderbird version
+  exit 1
+fi
 
 # Ubuntu's version of sed doesn't have -i
 sed -e "s/\"strict_max_version\": \".*\"/\"strict_max_version\": \"${THUNDERBIRD_VERSION}\"/" \
